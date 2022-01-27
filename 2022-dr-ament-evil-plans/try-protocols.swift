184ddf4cff9d002495eeae3c1ab0825f56004a75
Gabriel Maldonado <gabrielmaldonado.art@gmail.com>
2022-01-27 17:37:33 +0530

    Create try-protocols.swift

---
+++ 2022-dr-ament-evil-plans/try-protocols.swift
@@ -0,0 +1,69 @@
+/**
+* Abstracts objects that can be turned on and off
+*/
+protocol Switchable {
+    /*
+    * Turns the object on
+    */
+    func turnOn()
+
+    /*
+    * Turns the object off
+    */
+    func off()
+}
+
+protocol Hacked {
+    
+    func hackedLog(position: String)
+}
+
+// An autonomous Robot that can switch appliances on and off
+final class Robot {
+    func turnOn(appliance: Switchable) {
+        print("(*in a robotic voice*) Bleep blop bleep - I. shall. turn. on.");
+        appliance.turnOn();
+    }
+
+    func turnOff(appliance: Switchable) {
+        print("(*in a robotic voice*) Bleep blop bleep - turning. off.")
+        appliance.off()
+    }
+
+    func reboot() {
+        print("(*in a robotic voice*) Rebooting - I will be offline for 10 minutes.")
+    }
+}
+
+class LocationGateway: Hacked {
+    
+    func log(position: String) {
+        hackedLog(position: position)
+        print("Location logged encrypted: " + position)
+    }
+    
+    func hackedLog(position: String) {
+        print("Location logged completely un-encrypted, and leaked to the forces of good: " + position)
+    }
+
+}
+
+final class Rogers {
+    let gateway = LocationGateway()
+    func getLocationGateway() -> LocationGateway {
+        return gateway
+    }
+}
+
+final class EvilPlan {
+    static func execute() {
+        let robot = Robot()
+        let evilLogging = Rogers()
+        let locationLog = evilLogging.getLocationGateway()
+        
+        // turn something on / off
+        locationLog.log(position: "Ottawa. Switched a bunch of stuff off")
+    }
+}
+
+EvilPlan.execute()
